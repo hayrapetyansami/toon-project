6f72347a2469e2722a1a67537ac7b49f5512e14e
hayrapetyansami <samihayrapetyan@gmail.com>
2022-07-24 22:37:24 +0400

    created main section width items, fixed responsive and JS bugs

--- src/js/script.js
+++ src/js/script.js
@@ -13,7 +13,6 @@ window.addEventListener("DOMContentLoaded", () => {
 			navMenu.classList.add("active_nav");
 			topNavSwitch.innerHTML = "";
 			topNavSwitch.insertAdjacentHTML("afterbegin", logo.innerHTML);
-			console.log(topNavSwitch);
 			
 		} else {
 			menuBtn.classList.remove("open");
@@ -24,16 +23,33 @@ window.addEventListener("DOMContentLoaded", () => {
 	});
 
 	try {
-		scrollDown.addEventListener("click", (e) => {
-			main.scrollIntoView({
-				block: "start",
-				behavior: "smooth"
-			});
-			window.addEventListener("scroll", () => {
-				window.scrollY < main.offsetTop - 80 ? 
-				main.removeAttribute("style") : 
-				main.style.cssText = "padding-top: 100px";
+		scrollDown.addEventListener("click", () => {
+			window.scrollTo({
+				top: main.offsetTop - 105,
+				behavior: 'smooth'
 			});
 		});
 	} catch{}
+
+	try {
+		const item = document.querySelectorAll(".item");
+		const itemTitle = document.querySelectorAll(".item_footer h3 a");
+
+		item.forEach(item => {
+			item.setAttribute(
+				"title", 
+				item.children[1]
+				.children[0]
+				.textContent.trim()
+			);
+		})
+
+
+		itemTitle.forEach(title => {
+			if (title.textContent.length >= 20) {
+				title.textContent = title.textContent.slice(0, 20) + "...";
+			}
+		});
+		
+	} catch {}
 });
